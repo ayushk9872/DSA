78f11292b22dcbe618bc60dd3e4ee55d99ce354a
Ayush Kumar <ayushk119972@gmail.com>
2026-09-20 10:30:48 +0530

    Time: 0 ms (100.00%) | Memory: 9.7 MB (47.88%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.cpp
@@ -3,8 +3,8 @@ public:
     int reverseDegree(string s) {
         int sum=0;
         for(int i=0 ; i<s.length() ; i++){
-            int val=122-s[i]+1;
-            sum += val*(i+1);
+            
+            sum += (122-s[i]+1)*(i+1);
         }
         return sum;
         
